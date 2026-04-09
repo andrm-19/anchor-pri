5e3dc4b8b126c8e608c8d6991628351b52355b4b
andrm-19 <anfelipemartinez07@gmail.com>
2026-04-08 22:22:14 -0500

    Delete PRI°web directory

--- PRI°web/start-anchor-local.ps1
+++
@@ -1,3 +0,0 @@
-$root = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location (Join-Path $root "Anchor-Backend\Zenmode_api")
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
